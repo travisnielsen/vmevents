4f95ff183f0a6fc3e9e4508e9918efcceb72c906
Travis Nielsen <trniel@microsoft.com>
2018-01-09 12:46:04 -0600

    various updates

--- script/getevents.sh
+++ script/getevents.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
 events=$(curl -H Metadata:true http://169.254.169.254/metadata/scheduledevents?api-version=2017-08-01)
 
 # Sample JSON for testing when an event exists
 # events='{"DocumentIncarnation":0,"Events":[ { "EventId": "123", "EventType": "Redeploy", "ResourceType": "VirtualMachine", "ResourceName": "vm1234", "EventStatus": "Scheduled" } ] }'
 
 # Sample JSON for an empty result (no events)
-#events='{"DocumentIncarnation":0,"Events":[]}'
+# events='{"DocumentIncarnation":0,"Events":[]}'
 
-echo $events
+logger "getevents.sh: $events"
 eventCount=$(grep -ci '\[\]' <<< $events)
-echo $eventCount
+
 if (( $eventCount < 1 )); then
-    echo "Sending event data to Azure Function"
+    logger "vmevents.sh Sending event data to Azure Function"
     curl -H "Content-Type:application/json" --data "$events" https://vmevents.azurewebsites.net/api/SendEmail?code=igtLwOn2t4s69X4mozuh0uWd8i5jXEILOk6kakIQS0qO8IC0rs6j/Q==
+
+    # EXEUCTE YOUR APPLICATION STEPS HERE
+
 fi
--- script/setup.sh
+++ script/setup.sh
@@ -1,13 +1,15 @@
 # configure and download vm events script
-sudo mkdir /opt/vmevents
-cd vmevents
-sudo curl -o getevents.sh  https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/getevents.sh
-sudo chmod +x /opt/vmevents/getevents.sh
+mkdir /opt/vmevents
+curl -o /opt/vmevents/getevents.sh  https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/getevents.sh
+chmod +x /opt/vmevents/getevents.sh
 
 # download systemd service and task definitions
-sudo curl -o /usr/lib/systemd/system/ vmevents.service https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/vmevents.service
-sudo curl -o /usr/lib/systemd/system/ vmevents.timer https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/vmevents.timer
+curl -o /usr/lib/systemd/system/vmevents.service https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/vmevents.service
+curl -o /usr/lib/systemd/system/vmevents.timer https://raw.githubusercontent.com/travisnielsen/vmevents/master/script/vmevents.timer
 
 # set up schecduled task
 systemctl enable vmevents.timer
 systemctl start vmevents.timer
+
+# check status
+systemctl is-active vmevents.timer
